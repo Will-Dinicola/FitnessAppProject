54f1382ca951d954f93e6f2bef80cbb0538c8745
nhipham <nhivpham12@gmailcom>
2025-07-03 17:37:08 -0400

    Added the connection to the database in app.py

--- fitness-backend/app.py
+++ fitness-backend/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
+
+def get_db_connection():
+    connect = mysql.connector.connect(
+        host="localhost",
+        user="user_name",
+        password="user_password",
+        database="FitnessAppDB"
+    )
+    return connect
 
 @app.route("/")
 def hello_world():
