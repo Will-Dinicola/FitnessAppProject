64d41d7c8c995941dff0885fd9ce853996094090
nhipham <nhivpham12@gmailcom>
2025-07-03 20:24:04 -0400

    added ability to get data from & log exercises into DB

--- fitness-backend/app.py
+++ fitness-backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask import request
 import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
+
 
 def get_db_connection():
     connect = mysql.connector.connect(
@@ -14,23 +16,56 @@ def get_db_connection():
     )
     return connect
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
 
 @app.route("/api/data")
 def get_data():
     return jsonify({"message": "Data send via connection"})
 
+
 @app.route("/testdb")
 def test_db():
-    conn = get_db_connection()
-    cursor = conn.cursor()
+    connect = get_db_connection()
+    cursor = connect.cursor()
     cursor.execute("SHOW TABLES;")
     tables = cursor.fetchall()
     cursor.close()
-    conn.close()
+    connect.close()
     return {"tables": [t[0] for t in tables]}
+
+
+@app.route("/api/exercises", methods=["POST"])
+def add_exercise():
+    # get data sent to frontend
+    data = request.get_json()
+    workout_id = data.get("workout_id")  # track this in frontend
+    name = data.get("name")
+
+    # add stuff to record sets, reps, weight
+
+    # connect to DB
+    connect = get_db_connection()
+    cursor = connect.cursor()
+
+    # insert into DB
+    cursor.execute(
+        "INSERT INTO Exercises (workout_id, name) VALUES (%s, %s);",
+        (workout_id, name)
+    )
+
+    connect.commit()
+
+    # close connection
+    cursor.close()
+    connect.close()
+
+    # return success message
+    return jsonify({"message": "Exercise logged successfully!"})
+
 
 if __name__ == "__main__":
     app.run(debug=False)
